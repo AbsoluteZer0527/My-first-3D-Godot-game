38db6370d5403f350ec659ee0f1e542893956cdf
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2025-12-14 20:22:02 -0800

    Make Collectable into a separate scene, generate model based on according types

--- First-3D/new-game-project/Scripts/collectable.gd
+++ First-3D/new-game-project/Scripts/collectable.gd
@@ -1,7 +1,14 @@
 extends Area3D
 
-@export var rotation_speed: float = 0.5
+enum CollectibleType {DIAMOND, COIN, CHERRY}
+@export var type: CollectibleType
 
+@export var diamond_model: PackedScene
+@export var coin_model: PackedScene
+@export var cherry_model: PackedScene
+
+
+@export var rotation_speed: float = 0.5
 @export var floating_speed: float = 0.01
 @export var floating_magnitude: float = 0.05
 var original_y: float
@@ -9,8 +16,20 @@ var original_y: float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	original_y = position.y
-
-
+	
+	var model: PackedScene
+	match type: 
+		CollectibleType.DIAMOND:
+			model = diamond_model
+		CollectibleType.COIN:
+			model = coin_model
+		CollectibleType.CHERRY:
+			model = cherry_model
+		_:
+			printerr("Invalid Type!")
+	
+	var node = model.instantiate()
+	add_child(node)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotation.y += rotation_speed* delta
