d5b19629170407edd88fbc2ac3c81f6d16a15dc6
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2025-12-14 22:29:16 -0800

    Added new directory in Game Manager to keep track of collected item counts, collect_item now increment when items got collected

--- First-3D/new-game-project/Scripts/game_manager.gd
+++ First-3D/new-game-project/Scripts/game_manager.gd
@@ -3,13 +3,19 @@ class_name GameManager
 
 static var instance: GameManager
 
+@export var collected_items: Dictionary[String, int] = {
+		'DIAMOND': 0,
+		'COIN': 0,
+		'CHERRY': 0,
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if instance == null: 
 		instance = self
 	else:
 		queue_free()
-
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,4 +26,4 @@ func respawn_player(body: Node3D) -> void:
 		get_tree().reload_current_scene()
 
 func collect_item(item_type):
-	print(item_type)
+	collected_items[item_type] += 1
