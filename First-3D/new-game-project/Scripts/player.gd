72d4d9ac8fa11df6537de40fe4d0fd5985bcb005
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2025-12-20 15:32:20 -0800

    Finally, player movement direction is tied with camera direction. Fixed WASD movement bug.

--- First-3D/new-game-project/Scripts/player.gd
+++ First-3D/new-game-project/Scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-
+@export var camera: Camera3D
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,8 +16,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var input_dir := Input.get_vector("left", "right", "backward", "forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
