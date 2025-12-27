1a5668b3c062eb8b3dab3ca55e8badb127ce58fe
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2025-12-27 12:08:41 -0800

    Fixing type writing effect override bug

--- dialogue/Dialogues/UI/dialogue_manager.gd
+++ dialogue/Dialogues/UI/dialogue_manager.gd
@@ -18,21 +18,27 @@ func display_next_dialogue():
 	
 	var dialogue := main_dialogue.dialogue_list[dialogue_index]
 	
-	character_name_text.text = dialogue.character_name
-	
-	#display text like typewritter
-	typing_tween = get_tree().create_tween()
-	text_box.text = ""
-	for character in dialogue.content: 
-		typing_tween.tween_callback(append_character.bind(character)).set_delay(0.05)
-	
-	typing_tween.tween_callback(func(): dialogue_index +=1)
-	if dialogue.show_on_left:
-		left_avatar.texture = dialogue.avatar
-		right_avatar.texture = null
+	if typing_tween and typing_tween.is_running():
+		typing_tween.kill()
+		text_box.text = dialogue.content
+		dialogue_index +=1
 	else:
-		left_avatar.texture = null
-		right_avatar.texture = dialogue.avatar
+		
+		character_name_text.text = dialogue.character_name
+		
+		#display text like typewritter
+		typing_tween = get_tree().create_tween()
+		text_box.text = ""
+		for character in dialogue.content: 
+			typing_tween.tween_callback(append_character.bind(character)).set_delay(0.05)
+		
+		typing_tween.tween_callback(func(): dialogue_index +=1)
+		if dialogue.show_on_left:
+			left_avatar.texture = dialogue.avatar
+			right_avatar.texture = null
+		else:
+			left_avatar.texture = null
+			right_avatar.texture = dialogue.avatar
 		
 	
 func append_character(character:  String):
