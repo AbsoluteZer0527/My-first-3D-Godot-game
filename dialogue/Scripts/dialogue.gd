3a04fd3179e4c2257ee17d5e6481bb5dfa33fc36
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2025-12-27 11:54:10 -0800

    Debug/fixing typo to make the dialogue work

--- dialogue/Scripts/dialogue.gd
+++ dialogue/Scripts/dialogue.gd
@@ -2,6 +2,6 @@ extends Resource
 class_name Dialogue
 
 @export var character_name: String
-@export_multiline var concent: String
+@export_multiline var content: String
 @export var avatar: Texture
 @export var show_on_left: bool
