90a1fb7f8636bd2a580a2d424a1b88581057cda2
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2025-12-27 11:43:30 -0800

    Created basic structure for dialogue manager

---
+++ dialogue/Dialogues/UI/dialogue_manager.gd
@@ -0,0 +1,9 @@
+extends Control
+@export_group("UI")
+@export var character_name_text : Label
+@export var text_box : Label
+@export var left_avatar: TextureRect
+@export var right_avatar: TextureRect
+
+@export_group("Dialogue")
+@export var main_dialogue: DialogueGroup
