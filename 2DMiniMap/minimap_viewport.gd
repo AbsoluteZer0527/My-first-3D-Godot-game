c672934f438803df2d559fc6aadf4e060d127409
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2026-01-25 17:18:51 -0800

    Use Mini map background as separated UI for minimap

--- 2DMiniMap/minimap_viewport.gd
+++ 2DMiniMap/minimap_viewport.gd
@@ -6,7 +6,7 @@ extends SubViewport
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	world_2d = get_tree().root.world_2d
-
+	get_tree().root.set_canvas_cull_mask_bit(1,false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
